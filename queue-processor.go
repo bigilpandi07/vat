9155390f4ef744a1527d10668914cd70d3c24ca8
Ishan Jain <ishanjain28@gmail.com>
2017-07-18 21:21:16 +0530

    Now, Pauses loop with condition variables

--- queue-processor.go
+++ queue-processor.go
@@ -4,94 +4,102 @@ import (
 	"github.com/beeker1121/goque"
 	tbot "github.com/go-telegram-bot-api/telegram-bot-api"
 	"strconv"
-	"time"
+	"sync"
 )
 
 func startQueueProcessor(bot *tbot.BotAPI) {
+
+	s := &sync.Cond{L: &sync.Mutex{}}
+
 	for {
-		if item, err := q.Dequeue(); err == nil {
+		s.L.Lock()
 
-			var dj DownloadJob
-
-			err := item.ToObject(&dj)
-			if err != nil {
-				Error.Println("Error in converting item to object", err.Error())
-				continue
-			}
-			Info.Println("Processing", dj.DU.String())
-			Info.Println("Size", dj.SizeInMiB)
-
-			//Send a message to user as well
-			msg := tbot.NewMessage(dj.User.ChatID,
-				"Downloading "+
-					"\nName: "+ dj.Filename+
-					"\nLength: "+ strconv.FormatFloat(float64(dj.Size)/(1024*1024), 'f', 4, 64)+ "MiB"+
-					"\nType: "+ dj.ContentType+
-					"\nURL: "+ dj.DU.String())
-
-			bot.Send(msg)
-
-			//TODO:Process more than one items at a time if there is more space than what we need for the task at hand
-
-			err = dj.download()
-
-			if err != nil {
-				msg := tbot.NewMessage(dj.User.ChatID,
-					"Failed in Downloading " + dj.DU.String()+
-						"\nReason: "+ err.Error())
-
-				bot.Send(msg)
-				continue
-			}
-
-			msg = tbot.NewMessage(dj.User.ChatID,
-				"Calculating Hash "+
-					"\nName: "+ dj.Filename+
-					"\nLength: "+ strconv.FormatFloat(float64(dj.Size)/(1024*1024), 'f', 4, 64)+ "MiB"+
-					"\nType: "+ dj.ContentType+
-					"\nURL: "+ dj.DU.String())
-
-			bot.Send(msg)
-
-			err = dj.convert()
-			if err != nil {
-				msg = tbot.NewMessage(dj.User.ChatID,
-					"Error in Calculating Hash "+
-						"\nReason: "+ err.Error())
-				bot.Send(msg)
-
-				Warn.Println("Error in calculating Hash", err.Error())
-				continue
-			}
-
-			err = dj.save()
-			if err != nil {
-				msg := tbot.NewMessage(dj.User.ChatID, "Failed in Saving Hash "+dj.DU.String())
-				bot.Send(msg)
-
-				Warn.Println("Failed to save Hash", err.Error())
-				continue
-			}
-
-			//Remove the Downloaded file
-			err = dj.Clean()
-			if err != nil {
-				Error.Println("Error in cleaning", err.Error())
-			}
-
-			//Everything's Done! Send a Download link to User
-			msg = tbot.NewMessage(dj.User.ChatID, "Successful!"+
-				"\nLink: "+ HOST+ "/torrent/"+ dj.Metainfo.HashInfoBytes().String()+ ".torrent")
-			msg.ReplyToMessageID = dj.User.MessageID
-			bot.Send(msg)
-
-		} else {
-
+		item, err := q.Dequeue()
+		if err != nil {
 			if err != goque.ErrEmpty {
 				Error.Println("Error in Dequeueing", err.Error())
 			}
-			continue
-			time.After(time.Duration(2) * time.Second)
+
+			//	Pause loop
+			s.Wait()
 		}
+
+		//Resume
+		s.L.Unlock()
+
+		var dj DownloadJob
+
+		err = item.ToObject(&dj)
+		if err != nil {
+			Error.Println("Error in converting item to object", err.Error())
+			continue
+		}
+		Info.Println("Processing", dj.DU.String())
+		Info.Println("Size", dj.SizeInMiB)
+
+		//Send a message to user as well
+		msg := tbot.NewMessage(dj.User.ChatID,
+			"Downloading "+
+				"\nName: "+ dj.Filename+
+				"\nLength: "+ strconv.FormatFloat(float64(dj.Size)/(1024*1024), 'f', 4, 64)+ "MiB"+
+				"\nType: "+ dj.ContentType+
+				"\nURL: "+ dj.DU.String())
+
+		bot.Send(msg)
+
+		//TODO:Process more than one items at a time if there is more space than what we need for the task at hand
+
+		err = dj.download()
+
+		if err != nil {
+			msg := tbot.NewMessage(dj.User.ChatID,
+				"Failed in Downloading " + dj.DU.String()+
+					"\nReason: "+ err.Error())
+
+			bot.Send(msg)
+			continue
+		}
+
+		msg = tbot.NewMessage(dj.User.ChatID,
+			"Calculating Hash "+
+				"\nName: "+ dj.Filename+
+				"\nLength: "+ strconv.FormatFloat(float64(dj.Size)/(1024*1024), 'f', 4, 64)+ "MiB"+
+				"\nType: "+ dj.ContentType+
+				"\nURL: "+ dj.DU.String())
+
+		bot.Send(msg)
+
+		err = dj.convert()
+		if err != nil {
+			msg = tbot.NewMessage(dj.User.ChatID,
+				"Error in Calculating Hash "+
+					"\nReason: "+ err.Error())
+			bot.Send(msg)
+
+			Warn.Println("Error in calculating Hash", err.Error())
+			continue
+		}
+
+		err = dj.save()
+		if err != nil {
+			msg := tbot.NewMessage(dj.User.ChatID, "Failed in Saving Hash "+dj.DU.String())
+			bot.Send(msg)
+
+			Warn.Println("Failed to save Hash", err.Error())
+			continue
+		}
+
+		//Remove the Downloaded file
+		err = dj.Clean()
+		if err != nil {
+			Error.Println("Error in cleaning", err.Error())
+		}
+
+		//Everything's Done! Send a Download link to User
+		msg = tbot.NewMessage(dj.User.ChatID, "Successful!"+
+			"\nLink: "+ HOST+ "/torrent/"+ dj.Metainfo.HashInfoBytes().String()+ ".torrent")
+		msg.ReplyToMessageID = dj.User.MessageID
+		bot.Send(msg)
+
 	}
 }
